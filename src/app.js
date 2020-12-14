9703a475d3da28c4551882b3b39e30055fd6f7c5
MasonCrawford <MasonCrawford1994@gmail.com>
2020-12-14 15:34:59 +0800

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { kea, useValues, useActions } from "kea";
+
+const logic = kea({
+  // add your logic here
+});
+
+export default function App() {
+  // const {} = useValues(logic);
+  // const {} = useActions(logic);
+
+  return (
+    <div>
+      <img src="https://kea.js.org/img/logo.svg" alt="Kea Logo" />
+    </div>
+  );
+}
